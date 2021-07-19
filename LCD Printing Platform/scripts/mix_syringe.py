f059ff806ee29251d822debedec492b73bb02867
johnjubes <john.n.jubes@gmail.com>
2021-07-19 11:42:05 -0600

    added mix_syringe.py

---
+++ LCD Printing Platform/scripts/mix_syringe.py
@@ -0,0 +1,109 @@
+# File name: mix_syringe.py
+# Author: John Jubenville
+# Date created: 07/19/2021
+# Date last modified: 07/19/2021
+# Python Version: 3.9 / 2.7
+
+# Function:
+# Mixes bath by moving syringe to zero, to max, then back to start position
+
+# Setup:
+# Enter serial ID here
+serial_id = '/dev/ttyACM1'
+
+# Enter max rotations from 0 here
+max_rotations = 8
+
+import serial
+import time
+from reader import Reader
+
+def main():
+    # get layer data from layers.csv
+    reader = Reader()
+    current_syringe_pos = reader.getLast_syringe_pos()
+
+    #move back to zero
+    if (current_syringe_pos != 0):
+
+        move = current_syringe_pos * -1
+
+        #convert new_syringe_position to string message
+        out_str = str(move) + "\n"
+        out = bytes(out_str)
+
+        # Send new position to Arduino via serial, wait for move to finish
+        ################################################################## 
+        messages = 0
+        while messages < 2:
+            ser = serial.Serial('/dev/ttyACM1', 9600, timeout = 1)
+            ser.flush()
+            
+            ser.write(out)
+            
+            if ser.in_waiting > 0:
+                line = ser.readline().decode('utf-8').rstrip()
+		        #print(line)
+            messages = messages + 1
+            time.sleep(1)
+	    ##################################################################
+        
+        #Wait for move to finish
+        time.sleep(abs(int(move))*4)
+
+    #move to max
+    move = max_rotations
+
+    #convert new_syringe_position to string message
+    out_str = str(move) + "\n"
+    out = bytes(out_str)
+
+    # Send new position to Arduino via serial, wait for move to finish
+    ################################################################## 
+    messages = 0
+    while messages < 2:
+        ser = serial.Serial('/dev/ttyACM1', 9600, timeout = 1)
+        ser.flush()
+            
+        ser.write(out)
+            
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').rstrip()
+		    #print(line)
+        messages = messages + 1
+        time.sleep(1)
+	##################################################################
+        
+    #Wait for move to finish
+    time.sleep(abs(int(move))*4)
+
+    #Move back to position
+    if (max_rotations != current_syringe_pos):
+
+        #calulate move in rotations
+        move = current_syringe_pos - max_rotations
+
+        #convert new_syringe_position to string message
+        out_str = str(move) + "\n"
+        out = bytes(out_str)
+
+        # Send new position to Arduino via serial, wait for move to finish
+        ################################################################## 
+        messages = 0
+        while messages < 2:
+            ser = serial.Serial('/dev/ttyACM1', 9600, timeout = 1)
+            ser.flush()
+            
+            ser.write(out)
+            
+            if ser.in_waiting > 0:
+                line = ser.readline().decode('utf-8').rstrip()
+		        #print(line)
+            messages = messages + 1
+            time.sleep(1)
+	    ##################################################################
+        
+        #Wait for move to finish
+        time.sleep(abs(int(move))*4)
+
+main()
